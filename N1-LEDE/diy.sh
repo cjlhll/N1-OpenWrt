ba07a5509f99a9190c8bf4a6807b4ef8349e515d
AJ <983097239@qq.com>
2024-12-25 11:03:54 +0800

    Update diy.sh

--- N1-LEDE/diy.sh
+++ N1-LEDE/diy.sh
@@ -37,8 +37,7 @@ git clone https://github.com/sbwml/luci-app-alist package/alist
 git clone https://github.com/chenhw2/luci-app-aliddns.git package/luci-app-aliddns
 
 # => mihomo
-#git clone https://github.com/morytyann/OpenWrt-mihomo package/mihomo
-echo "src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main" >> "feeds.conf.default"
+git clone https://github.com/morytyann/OpenWrt-mihomo package/mihomo
 
 #修改默认时间格式
 sed -i 's/os.date()/os.date("%Y-%m-%d %H:%M:%S %A")/g' $(find ./package/*/autocore/files/ -type f -name "index.htm")
